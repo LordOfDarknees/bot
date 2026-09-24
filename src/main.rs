b93bd4364ae736d3970eea41217514d5a1128849
LordOfDarkness <cajusneumann@gmail.com>
2026-09-24 10:59:26 +0200

    changed var from constant to immutable variable wich will be disposed

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::env;
 use dotenvy::from_filename;
 use serenity::futures::future::ok;
 
-const config_file_name: &'static str = "config.env";
+// const config_file_name: &'static str = "config.env";
 
 fn main() {
+    let config_file_name: &str = "config.env";
+
     from_filename(config_file_name).expect("File not found");
     let token_key: &'static str = "discord_token";
     let token: Result<String, env::VarError> = env::var(token_key);
