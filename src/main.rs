490f894ad17668f87f1da2ffe553576c157e8be1
LordOfDarkness <cajusneumann@gmail.com>
2026-09-24 10:56:00 +0200

    env loadable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use core::str;
+use std::env;
+use dotenvy::from_filename;
+use serenity::futures::future::ok;
+
+const config_file_name: &'static str = "config.env";
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hi")
+    from_filename(config_file_name).expect("File not found");
+    let token_key: &'static str = "discord_token";
+    let token: Result<String, env::VarError> = env::var(token_key);
+    
+    println!("Token: {:?}", token);
 }
