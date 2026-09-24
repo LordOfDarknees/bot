7b6074d2a1d65f1c774c12a74908bab2336022ce
LordOfDarkness <cajusneumann@gmail.com>
2026-09-24 12:11:23 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use core::str;
-use std::env;
+use std::{env::{self, VarError}};
 use dotenvy::from_filename;
 use serenity::futures::future::ok;
 
 // const config_file_name: &'static str = "config.env";
 
-fn main() {
+fn get_token() -> Result<String, env::VarError> {
+
     let config_file_name: &str = "config.env";
 
     from_filename(config_file_name).expect("File not found");
     let token_key: &str = "discord_token";
     let token: Result<String, env::VarError> = env::var(token_key);
-    
-    println!("Token: {:?}", token);
+    return token;
+}
+
+fn main() {
+    let retrieved_token: Result<String, VarError> = get_token();
+    match retrieved_token {
+        Ok(v) => println!("Token: {:?}", v),
+        Err(e) => panic!("Token not found. {:?}", e)
+    }
 }
